c4ea7b3ac3003e126e0f4606732fa99eda1012fb
Anu Nikkanen <anu.nikkanen@gmail.com>
2013-08-08 21:17:55 +0300

    lisatty dokumentointi-kansio

---
+++ Muistipeli/src/muistipeli/sovelluslogiikka/Laskuri.java
@@ -0,0 +1,15 @@
+
+package muistipeli.sovelluslogiikka;
+
+/**
+ * Ohjelmoinnin harjoitustyö, syksy 2013
+ * TÄTÄ PITÄÄ MUOKATA UUTEEN SOVELLUSLOGIIKKAAN SOVELTUVAKSI.
+ * 
+ * @author Anu Nikkanen
+ */
+public interface Pistelaskuri {
+    public int annaArvo();
+    public void kasvataArvoa();   
+    public int annaKierrokset();
+    public void kasvataKierrosta();
+}
--- Muistipeli/src/muistipeli/sovelluslogiikka/Pistelaskuri.java
+++ Muistipeli/src/muistipeli/sovelluslogiikka/Pistelaskuri.java
@@ -3,13 +3,49 @@ package muistipeli.sovelluslogiikka;
 
 /**
  * Ohjelmoinnin harjoitustyö, syksy 2013
- * TÄTÄ PITÄÄ MUOKATA UUTEEN SOVELLUSLOGIIKKAAN SOVELTUVAKSI.
+ * Pistelaskuria ei tällä hetkellä vielä kytketty sovellukseen. Työn alla...
  * 
  * @author Anu Nikkanen
  */
-public interface Pistelaskuri {
-    public int annaArvo();
-    public void kasvataArvoa();   
-    public int annaKierrokset();
-    public void kasvataKierrosta();
+public class OmaPistelaskuri implements Pistelaskuri {
+    
+    private int arvo;
+    private int lkm; // jos jää tälleen, niin tee tälle oma luokka -> ei copypastea!
+    
+    public OmaPistelaskuri() {
+        this.arvo = 0;
+        this.lkm = 0;
+    }
+    
+    @Override
+    public int annaArvo() {
+        return this.arvo;
+    }
+
+    @Override
+    public void kasvataArvoa() {
+        this.arvo++; // tämä pitää muuttaa vielä myöhemmin oikein toimivaksi,
+        // eli niin, että on myös toinen laskuri (pisteidenLaskija), johon tämä viittaa.. eli mitä
+        // vähemmän on arvauksia, niin sitä paremmat pisteet
+    }
+    // lisätäänkö rajapintaan??
+    public int pisteidenLaskija(int arvo) {
+        // tämän laskurin on tarkoitus pitää kirjaa arvauksista. tähän joku sellainen
+        // logiikka, että pelaaja saa eri määrän pisteitä esim. riippuen siitä,
+        // kuinka monta kierrosta on mennyt 
+        return 0;
+    } 
+
+    @Override
+    public int annaKierrokset() { // väliaikainen ratkaisu
+        return this.lkm;
+    }
+
+    @Override
+    public void kasvataKierrosta() { // väliaikainen ratkaisu
+        this.lkm++;
+    }
+    
+    
 }
+
